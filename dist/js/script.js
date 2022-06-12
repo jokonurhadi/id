66c7ab37ba6dd45e821faf4fcfc1ca3b22198f7e
JokoNurHadi <richmediagroupcorp@gmail.com>
2022-06-12 13:29:04 +0700

    first commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,14 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header .offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
     }
 };
 
